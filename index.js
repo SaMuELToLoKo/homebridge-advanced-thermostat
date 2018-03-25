10d6d17d81d2ad1b0286e9fa71931726351efca3
SaMuELToLoKo <37693787+SaMuELToLoKo@users.noreply.github.com>
2018-03-25 14:14:49 +0100

    index.js 1.0.4

--- index.js
+++ index.js
@@ -193,10 +193,10 @@ Thermostat.prototype = {
 					this.log('Error setting Target State: %s', err);
 					return callback("Error setting mode: " + err);
 				}
-			}).bind(this));
-		},
+		}).bind(this));
+	},
 
-		getCurrentTemperature: function(callback) {
+	getCurrentTemperature: function(callback) {
 			this.log('Getting Current Temperature from: ', this.apiAdress + '/info');
 			return request.get({
 				url: this.apiAdress + '/info',
@@ -207,24 +207,24 @@ Thermostat.prototype = {
 			}, (function(err, response, body) {
 				var json;
 				if (!err && response.statusCode === 200) {
-	        this.log('response success');
-	        json = JSON.parse(body);
-	        this.log('Currente Temperature is %s (%s)', json.temperature, json.units);
-					if (json.units == 0){
-						this.CurrentTemperature = parseFloat(json.temperature);
+	        			this.log('response success');
+	        			json = JSON.parse(body);
+	        			this.log('Currente Temperature is %s (%s)', json.temperature, json.units);
+				if (json.units == 0){
+					this.CurrentTemperature = parseFloat(json.temperature);
 					}
-					else if (json.units == 1) {
+				else if (json.units == 1) {
 					this.CurrentTemperature = this.cToF(parseFloat(json.temperature));
 				}
-	        return callback(null, this.CurrentTemperature);
-	      } else {
-	        this.log('Error getting current temp: %s', err);
-	        return callback("Error getting current temp: " + err);
-	      }
-	    }).bind(this));
+	        		return callback(null, this.CurrentTemperature);
+	      			} else {
+	        		this.log('Error getting current temp: %s', err);
+	        		return callback("Error getting current temp: " + err);
+	      			}
+	    	}).bind(this));
 	  },
 
-		getTargetTemperature: function(callback) {
+	getTargetTemperature: function(callback) {
 			this.log('Getting Target Temperature from: ', this.apiAdress + '/info');
 			return request.get({
 				url: this.apiAdress + '/info',
@@ -235,9 +235,9 @@ Thermostat.prototype = {
 			}, (function(err, response, body) {
 				var json;
 				if (!err && response.statusCode === 200) {
-        this.log('response success');
-				json = JSON.parse(body);
-				this.log('Target Temperature is %s (%s)', json.tarTemperature, json.units);
+        				this.log('response success');
+					json = JSON.parse(body);
+					this.log('Target Temperature is %s (%s)', json.tarTemperature, json.units);
 				if (json.units == 0) {
 					this.TargetTemperature = parseFloat(json.tarTemperature);
 				}
@@ -245,12 +245,12 @@ Thermostat.prototype = {
 					this.TargetTemperature = this.cToF(parseFloat(json.tarTemperature));
 				}
 				return callback(null, this.TargetTemperature);
-			} else {
-				this.log('Error getting target temp: %s', err);
-				return callback("Error getting target temp: " + err);
+				} else {
+					this.log('Error getting target temp: %s', err);
+					return callback("Error getting target temp: " + err);
 				}
-			}).bind(this));
-  },
+		}).bind(this));
+ 	},
 
 	setTargetTemperature: function(value, callback) {
 			if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.FAHRENHEIT) {
@@ -285,187 +285,187 @@ Thermostat.prototype = {
 		}, (function(err, response, body) {
 			var json;
 			if (!err && response.statusCode === 200) {
-			this.log('response success');
-			json = JSON.parse(body);
-			this.log('Target Heat threshoold is %s (%s)', json.heatThreshold, json.units);
-			if (json.units == 0) {
-				this.HeatingThresholdTemperature = parseFloat(json.heatThreshold);
-			}
-			else if (json.units == 1) {
-				this.HeatingThresholdTemperature = this.cToF(parseFloat(json.heatThreshold));
-			}
-			return callback(null, this.HeatingThresholdTemperature);
-		} else {
-			this.log('Error getting target Heat threshold: %s', err);
-			return callback("Error getting target heat threshold: " + err);
-			}
+				this.log('response success');
+				json = JSON.parse(body);
+				this.log('Target Heat threshoold is %s (%s)', json.heatThreshold, json.units);
+				if (json.units == 0) {
+					this.HeatingThresholdTemperature = parseFloat(json.heatThreshold);
+				}
+				else if (json.units == 1) {
+					this.HeatingThresholdTemperature = this.cToF(parseFloat(json.heatThreshold));
+				}
+				return callback(null, this.HeatingThresholdTemperature);
+				} else {
+					this.log('Error getting target Heat threshold: %s', err);
+					return callback("Error getting target heat threshold: " + err);
+				}
 		}).bind(this));
-},
+	},
 
-setHeatingThresholdTemperature: function(value, callback) {
-		if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.FAHRENHEIT) {
-			value = cToF(value);
-			}
-		this.log('Setting Target Heat Threshold from: ', this.apiAdress + '/heatThreshold/' + value);
-		return request.post({
+	setHeatingThresholdTemperature: function(value, callback) {
+			if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.FAHRENHEIT) {
+				value = cToF(value);
+				}
+			this.log('Setting Target Heat Threshold from: ', this.apiAdress + '/heatThreshold/' + value);
+			return request.post({
 				url: this.apiAdress + '/heatThreshold/' + value,
 				auth: {
 					user: this.username,
 					pass: this.password
 			}
-		}, (function(err, response, body) {
-		if (!err && response.statusCode === 200) {
-		this.log('response success');
-		return callback(null);
-		} else {
-		this.log('Error getting heat threshold: %s', err);
-		return callback("Error setting target heat threshold: " + err);
-		}
-	}).bind(this));
-},
+			}, (function(err, response, body) {
+			if (!err && response.statusCode === 200) {
+				this.log('response success');
+				return callback(null);
+			} else {
+				this.log('Error getting heat threshold: %s', err);
+				return callback("Error setting target heat threshold: " + err);
+			}
+		}).bind(this));
+	},
 
-getCoolingThresholdTemperature: function(callback) {
-	this.log('Getting Cool Threshold from: ', this.apiAdress + '/info');
-	return request.get({
-		url: this.apiAdress + '/info',
-		auth: {
-			user: this.username,
-			pass: this.password
-		}
-	}, (function(err, response, body) {
-		var json;
-		if (!err && response.statusCode === 200) {
-		this.log('response success');
-		json = JSON.parse(body);
-		this.log('Target Cool threshold is %s (%s)', json.coolThreshold, json.units);
-		if (json.units == 0) {
-			this.CoolingThresholdTemperature = parseFloat(json.coolThreshold);
-		}
-		else if (json.units == 1) {
-			this.CoolingThresholdTemperaturee = this.cToF(parseFloat(json.coolThreshold));
-		}
-		return callback(null, this.CoolingThresholdTemperature);
-	} else {
-		this.log('Error getting target Cool threshold: %s', err);
-		return callback("Error getting target cool threshold: " + err);
-		}
-	}).bind(this));
-},
-
-setCoolingThresholdTemperature: function(value, callback) {
-	if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.FAHRENHEIT) {
-		value = cToF(value);
-		}
-	this.log('Setting Target Cool Threshold from: ', this.apiAdress + '/coolThreshold/' + value);
-	return request.post({
-			url: this.apiAdress + '/coolThreshold/' + value,
-			auth: {
+	getCoolingThresholdTemperature: function(callback) {
+			this.log('Getting Cool Threshold from: ', this.apiAdress + '/info');
+			return request.get({
+				url: this.apiAdress + '/info',
+				auth: {
 				user: this.username,
 				pass: this.password
-		}
-	}, (function(err, response, body) {
-	if (!err && response.statusCode === 200) {
-	this.log('response success');
-	return callback(null);
-	} else {
-	this.log('Error getting cool threshold: %s', err);
-	return callback("Error setting target cool threshold: " + err);
-	}
-}).bind(this));
-},
-
-			getTemperatureDisplayUnits: function(callback) {
-					this.log('Getting Temperature Display Units from: ', this.apiAdress + '/info');
-					return request.get({
-						url: this.apiAdress + '/info',
-						auth: {
-							user: this.username,
-							pass: this.password
-						}
-					}, (function(err, response, body) {
-						var json;
-						if (!err && response.statusCode === 200) {
-			this.log('response success');
-			json = JSON.parse(body);
-			this.log('Temperature Display Units %s', json.units);
-			if (json.units == 0) {
-				this.TemperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.CELSIUS;
 			}
-			else if (json.units == 1) {
-				this.TemperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.FAHRENHEIT;
-			}
-			return callback(null, this.TemperatureDisplayUnits);
-		} else {
-			this.log('Error getting Temperature Display Units', err);
-			return callback("Error getting Temperature Display Units: " + err);
-			}
-		}).bind(this));
-	},
-
-			setTemperatureDisplayUnits: function(value, callback) {
-				this.log('Setting Temperature Display Units from/to ', this.TemperatureDisplayUnits, value);
-				return request.post({
-					url: this.apiAdress + '/units/' + value,
-					auth: {
-						user: this.username,
-						pass: this.password
-					}
-				}, (function(err, response, body) {
+			}, (function(err, response, body) {
+				var json;
 				if (!err && response.statusCode === 200) {
-		 		this.log('response success');
-		 	return callback(null);
-	 		} else {
-		 	this.log('Error getting state: %s', err);
-		 	return callback("Error setting target temp: " + err);
-	 		}
+					this.log('response success');
+					json = JSON.parse(body);
+					this.log('Target Cool threshold is %s (%s)', json.coolThreshold, json.units);
+					if (json.units == 0) {
+						this.CoolingThresholdTemperature = parseFloat(json.coolThreshold);
+					}
+					else if (json.units == 1) {
+						this.CoolingThresholdTemperaturee = this.cToF(parseFloat(json.coolThreshold));
+					}
+					return callback(null, this.CoolingThresholdTemperature);
+					} else {
+						this.log('Error getting target Cool threshold: %s', err);
+						return callback("Error getting target cool threshold: " + err);
+					}
+			}).bind(this));
+		},
+
+	setCoolingThresholdTemperature: function(value, callback) {
+			if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.FAHRENHEIT) {
+				value = cToF(value);
+			}
+			this.log('Setting Target Cool Threshold from: ', this.apiAdress + '/coolThreshold/' + value);
+			return request.post({
+				url: this.apiAdress + '/coolThreshold/' + value,
+				auth: {
+					user: this.username,
+					pass: this.password
+			}
+			}, (function(err, response, body) {
+			if (!err && response.statusCode === 200) {
+				this.log('response success');
+				return callback(null);
+			} else {
+				this.log('Error getting cool threshold: %s', err);
+				return callback("Error setting target cool threshold: " + err);
+			}
 		}).bind(this));
 	},
 
-			getCurrentRelativeHumidity: function(callback) {
-	 			this.log('getCurrentRelativeHumidity from:', this.apiAdress + '/info');
-				return request.get({
-					url: this.apiAdress + '/info',
-					auth: {
-						user: this.username,
-						pass: this.password
+	getTemperatureDisplayUnits: function(callback) {
+			this.log('Getting Temperature Display Units from: ', this.apiAdress + '/info');
+			return request.get({
+				url: this.apiAdress + '/info',
+				auth: {
+					user: this.username,
+					pass: this.password
+				}
+				}, (function(err, response, body) {
+					var json;
+					if (!err && response.statusCode === 200) {
+						this.log('response success');
+						json = JSON.parse(body);
+						this.log('Temperature Display Units %s', json.units);
+						if (json.units == 0) {
+							this.TemperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.CELSIUS;
+						}
+						else if (json.units == 1) {
+							this.TemperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.FAHRENHEIT;
+						}
+						return callback(null, this.TemperatureDisplayUnits);
+						} else {
+							this.log('Error getting Temperature Display Units', err);
+							return callback("Error getting Temperature Display Units: " + err);
+						}
+		}).bind(this));
+	},
+
+	setTemperatureDisplayUnits: function(value, callback) {
+			this.log('Setting Temperature Display Units from/to ', this.TemperatureDisplayUnits, value);
+			return request.post({
+				url: this.apiAdress + '/units/' + value,
+				auth: {
+					user: this.username,
+					pass: this.password
+				}
+				}, (function(err, response, body) {
+					if (!err && response.statusCode === 200) {
+		 				this.log('response success');
+		 				return callback(null);
+	 				} else {
+		 			this.log('Error getting state: %s', err);
+		 			return callback("Error setting target temp: " + err);
+	 				}
+			}).bind(this));
+	},
+
+	getCurrentRelativeHumidity: function(callback) {
+	 		this.log('getCurrentRelativeHumidity from:', this.apiAdress + '/info');
+			return request.get({
+				url: this.apiAdress + '/info',
+				auth: {
+					user: this.username,
+					pass: this.password
 					}
 			}, (function(err, response, body) {
 				var json;
-			if (!err && response.statusCode === 200) {
-		        	this.log('response success');
-		        	json = JSON.parse(body);
-		        	this.log('Currente Humidity is %s', json.humidity);
-				this.CurrentRelativeHumidity = parseFloat(json.humidity);
-		        	return callback(null, this.CurrentRelativeHumidity);
-		      	} else {
-		        this.log('Error getting current humidity: %s', err);
-		        return callback("Error getting current hum: " + err);
-		      }
-		   }).bind(this));
-		 },
+				if (!err && response.statusCode === 200) {
+		        		this.log('response success');
+		        		json = JSON.parse(body);
+		        		this.log('Currente Humidity is %s', json.humidity);
+					this.CurrentRelativeHumidity = parseFloat(json.humidity);
+		        		return callback(null, this.CurrentRelativeHumidity);
+		      		} else {
+		        		this.log('Error getting current humidity: %s', err);
+		        		return callback("Error getting current hum: " + err);
+		      		}
+		}).bind(this));
+	},
 
- 			getTargetRelativeHumidity: function(callback) {
-	 			var error;
-	 			this.log('Get humidity unsupported');
-	 			error = "Get humidity unsupported";
-	 			return callback(error);
- 				},
+ 	getTargetRelativeHumidity: function(callback) {
+	 		var error;
+	 		this.log('Get humidity unsupported');
+	 		error = "Get humidity unsupported";
+			return callback(error);
+	},
 
- 			setTargetRelativeHumidity: function(value, callback) {
-	 			var error;
-	 			this.log('Set humidity unsupported');
-	 			error = "Set humidity unsupported";
-	 			return callback(error);
- 				},
+ 	setTargetRelativeHumidity: function(value, callback) {
+	 		var error;
+	 		this.log('Set humidity unsupported');
+	 		error = "Set humidity unsupported";
+			return callback(error);
+	},
 
- 			getName: function(callback) {
-	 			var error;
-	 			this.log('getName :', this.name);
-	 			error = null;
-	 			return callback(error, this.name);
- 			},
+ 	getName: function(callback) {
+			var error;
+	 		this.log('getName :', this.name);
+	 		error = null;
+	 		return callback(error, this.name);
+ 	},
 
-			getServices: function() {
+	getServices: function() {
 
 				var informationService = new Service.AccessoryInformation();
 
