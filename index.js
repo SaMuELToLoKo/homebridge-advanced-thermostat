00dd66b29c611cce818f44efc86818ef2f07f606
John Burwell <john@b-wells.us>
2019-01-02 17:30:36 -0600

    add support for temperature display units
    also some fiddling

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// Homebridge plugin for thermostats supported by Infinitive
+// Homebridge plugin for thermostats supported by Infinitive <https://github.com/acd/infinitive/>
 // Author John Burwell
 // Version 0.0.1	
-//
-// Adds support for Infinitive to Homebridge
 
 
 var Service, Characteristic;
@@ -17,37 +15,40 @@ module.exports = function(homebridge) {
 
 function Thermostat(log, config) {
 
-	var dispCelsius = Characteristic.TemperatureDisplayUnits.CELSIUS;
-	var dispFahrenheit = Characteristic.TemperatureDisplayUnits.FAHRENHEIT;
+	this.log = log;
 
 	// Plugin configuration
-	this.log = log;
-	this.apiAddress = config["apiAddress"];
+	this.apiEndpoint = config["apiEndpoint"];
 	this.name = config["name"] || "Thermostat";
-	this.manufacturer = config["manufacturer"] || "User-Thermostat";
-	this.model = config["model"] ||	"Homebridge-Thermostat";
+	this.manufacturer = config["manufacturer"] || "Homebridge";
+	this.model = config["model"] ||	"Infinitive";
 	this.serial_number = config["serial_number"] ||	"XXX.XXX.XXX.XXX";
 
-	// Thermostat configuration
+	// Thermostat state initialization
 	this.CurrentHeatingCoolingState = Characteristic.CurrentHeatingCoolingState.OFF;
 	this.TargetHeatingCoolingState = Characteristic.TargetHeatingCoolingState.OFF;
-	this.CurrentTemperature = 20;
+	this.CurrentTemperature = 20; // in degrees Celsius, as required by Homekit
 	this.TargetTemperature = 20;
 	this.CurrentRelativeHumidity = 20;
-	this.CurrentFanState = Characteristic.CurrentFanState.INACTIVE;
 	this.CoolingThresholdTemperature = 25;
 	this.HeatingThresholdTemperature = 20;
+	this.TemperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.FAHRENHEIT;
+	this.CurrentFanState = Characteristic.CurrentFanState.INACTIVE;
+	this.CurrentFanActive = Characteristic.Active.INACTIVE;
+	this.CurrentFilterLifeLevel = Characteristic.FilterLifeLevel.;
 
 	// Infinitive API configuration
-	this.username = config["username"] ||	"";
-	this.password = config["password"] ||	"";
-	this.TemperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.FAHRENHEIT;
-	this.maxTemp = config["maxTemp"] ||	38;
-	this.minTemp = config["minTemp"] ||	10;
-	this.log(this.name, this.apiAddress);
+	this.username = config["username"] || "";
+	this.password = config["password"] || "";
+	this.maxTemp = config["maxTemp"] ||	32; // Maximum allowed by the thermostat
+	this.minTemp = config["minTemp"] ||	5; // Minimum allowed by the thermostat
+	
+	this.log(this.name, this.apiEndpoint);
 
+	// Instantiate services
 	this.ThermostatService = new Service.Thermostat(this.name);
-	this.FanService = new Service.Fan(this.name);
+	this.FanService = new Service.Fanv2(this.name);
+	this.FilterMaintenanceService = new Service.FilterMaintenance(this.name);
 
 }
 
@@ -63,13 +64,16 @@ Thermostat.prototype = {
 
 	identify: function(callback) {
 		this.log('Identify requested');
+
+		// We could set the LCD brightness briefly?
+
 		return callback(); // success
 	},
 
 	getCurrentHeatingCoolingState: function(callback) {
-		this.log('Getting current mode from: ', this.apiAddress + '/config');
+		this.log('Getting current mode from: ', this.apiEndpoint + '/zone/1/config');
 		return request.get({
-			url: this.apiAddress + '/config',
+			url: this.apiEndpoint + '/zone/1/config',
 			auth: {
 				user: this.username,
 				pass: this.password
@@ -102,9 +106,9 @@ Thermostat.prototype = {
 	},
 
 	getTargetHeatingCoolingState: function(callback) {
-		this.log('Getting target mode from: ', this.apiAddress + '/config');
+		this.log('Getting target mode from: ', this.apiEndpoint + '/zone/1/config');
 		return request.get({
-			url: this.apiAddress + '/config',
+			url: this.apiEndpoint + '/zone/1/config',
 			auth: {
 				user: this.username,
 				pass: this.password
@@ -138,7 +142,7 @@ Thermostat.prototype = {
 
 	setTargetHeatingCoolingState: function(value, callback) {
 		var tarState = "";
-		this.log('Setting target mode from/to', this.TargetHeatingCoolingState, value);
+		this.log('Setting target mode from/to ', this.TargetHeatingCoolingState, value);
 		if (value == Characteristic.TargetHeatingCoolingState.OFF) {
 			this.TargetHeatingCoolingState = Characteristic.TargetHeatingCoolingState.OFF;
 			tarState = "off";
@@ -161,7 +165,7 @@ Thermostat.prototype = {
 			return callback(value + " mode unsupported");
 		}
 		return request.put({
-			url: this.apiAddress + '/config',
+			url: this.apiEndpoint + '/zone/1/config',
 			json: {
 				mode: tarState
 			},
@@ -181,9 +185,9 @@ Thermostat.prototype = {
 	},
 
 	getCurrentTemperature: function(callback) {
-		this.log('Getting current temperature from: ', this.apiAddress + '/config');
+		this.log('Getting current temperature from: ', this.apiEndpoint + '/zone/1/config');
 		return request.get({
-			url: this.apiAddress + '/config',
+			url: this.apiEndpoint + '/zone/1/config',
 			auth: {
 			user: this.username,
 			pass: this.password
@@ -193,12 +197,12 @@ Thermostat.prototype = {
 			if (!err && response.statusCode === 200) {
 				this.log('response success');
 				json = JSON.parse(body);
-				if (json.units == 0){
-					this.log('Current temperature in degrees Celsius is %s (%s)', json.currentTemp, json.units);
+				if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.CELSIUS){
+					this.log('Current temperature in degrees Celsius is %s (%s)', json.currentTemp, this.TemperatureDisplayUnits);
 					this.CurrentTemperature = parseFloat(json.currentTemp);
 				}
-				else if (json.units == 1) {
-					this.log('Current temperature in degrees Fahrenheit is %s (%s)', json.currentTemp, json.units);
+				else if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.FAHRENHEIT){
+					this.log('Current temperature in degrees Fahrenheit is %s (%s)', json.currentTemp, this.TemperatureDisplayUnits);
 					this.CurrentTemperature = this.fToC(parseFloat(json.currentTemp));
 				}
 			return callback(null, this.CurrentTemperature);
@@ -210,25 +214,41 @@ Thermostat.prototype = {
 	},
 
 	getTargetTemperature: function(callback) {
-		this.log('Getting target temperature from: ', this.apiAddress + '/config');
+		this.log('Getting target temperature from: ', this.apiEndpoint + '/zone/1/config');
 		return request.get({
-			url: this.apiAddress + '/config',
+			url: this.apiEndpoint + '/zone/1/config',
 			auth: {
 				user: this.username,
 				pass: this.password
 			}
 		}, (function(err, response, body) {
 			var json;
+			var tarTemp;
 			if (!err && response.statusCode === 200) {
 				this.log('response success');
 			json = JSON.parse(body);
-			if (json.units == 0) {
-				this.log('Target temperature in degrees Celsius is %s (%s)', json.currentTemp, json.units);
-				this.TargetTemperature = parseFloat(json.currentTemp);
+
+			switch (this.TargetHeatingCoolingState) {
+				case Characteristic.TargetHeatingCoolingState.HEAT:
+					tarTemp = json.heatSetpoint;
+					break;
+				case Characteristic.TargetHeatingCoolingState.COOL:
+					tarTemp = json.coolSetpoint;
+					break;
+				case Characteristic.TargetHeatingCoolingState.AUTO:
+					tarTemp = (json.coolSetpoint + json.heatSetpoint) / 2;
+					break;
+				default:
+					break;
 			}
-			else if (json.units == 1) {
-				this.log('Target temperature in degrees Fahrenheit is %s (%s)', json.currentTemp, json.units);
-				this.TargetTemperature = this.fToC(parseFloat(json.currentTemp));
+
+			if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.CELSIUS) {
+				this.log('Target temperature in degrees Celsius is %s (%s)', tarTemp, this.TemperatureDisplayUnits);
+				this.TargetTemperature = parseFloat(tarTemp);
+			}
+			else if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.FAHRENHEIT) {
+				this.log('Target temperature in degrees Fahrenheit is %s (%s)', tarTemp, this.TemperatureDisplayUnits);
+				this.TargetTemperature = this.fToC(parseFloat(tarTemp));
 			}
 			return callback(null, this.TargetTemperature);
 			} else {
@@ -240,29 +260,34 @@ Thermostat.prototype = {
 
 	setTargetTemperature: function(value, callback) {
 			if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.FAHRENHEIT) {
+				// in other words, we are in C because of Homekit, so if the stat is in F, we need to convert
 				value = this.cToF(value);
 				}
-			this.log('Setting target temperature from: ', this.apiAddress + '/config');
+			this.log('Setting target temperature from: ', this.apiEndpoint + '/zone/1/config');
 			
 			var modeJson = JSON;
 			
 			switch (this.TargetHeatingCoolingState) {
 				case Characteristic.TargetHeatingCoolingState.HEAT:
 					modeJson.heatSetpoint = value;
+					this.log('Heat setpoint: ', value);
 					break;
 				case Characteristic.TargetHeatingCoolingState.COOL:
 					modeJson.coolSetpoint = value;
+					this.log('Cool setpoint: ', value);
 					break;
 				case Characteristic.TargetHeatingCoolingState.AUTO:
 					modeJson.coolSetpoint = (value + 2);
+					this.log('Auto cool setpoint: ', value);
 					modeJson.heatSetpoint = (value - 3);
+					this.log('Auto heat setpoint: ', value);
 					break;
 				default:
 					break;
 			}
 
 			return request.put({
-				url: this.apiAddress + '/config',
+				url: this.apiEndpoint + '/zone/1/config',
 				json: modeJson,
 				auth: {
 					user: this.username,
@@ -280,24 +305,25 @@ Thermostat.prototype = {
 	},
 
 	getHeatingThresholdTemperature: function(callback) {
-		this.log('Getting heating threshold from: ', this.apiAddress + '/config');
+		this.log('Getting heating threshold from: ', this.apiEndpoint + '/zone/1/config');
 		return request.get({
-			url: this.apiAddress + '/config',
+			url: this.apiEndpoint + '/zone/1/config',
 			auth: {
 				user: this.username,
 				pass: this.password
 			}
 		}, (function(err, response, body) {
 			var json;
+
 			if (!err && response.statusCode === 200) {
 				this.log('response success');
 				json = JSON.parse(body);
-				if (json.units == 0) {
-					this.log('Target heat threshold in degrees Celsius is %s (%s)', json.heatSetpoint, json.units);
+				if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.CELSIUS) {
+					this.log('Target heat threshold in degrees Celsius is %s (%s)', json.heatSetpoint, this.TemperatureDisplayUnits);
 					this.HeatingThresholdTemperature = parseFloat(json.heatSetpoint);
 				}
-				else if (json.units == 1) {
-					this.log('Target heat threshold in degrees Fahrenheit is %s (%s)', json.heatSetpoint, json.units);
+				else if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.FAHRENHEIT) {
+					this.log('Target heat threshold in degrees Fahrenheit is %s (%s)', json.heatSetpoint, this.TemperatureDisplayUnits);
 					this.HeatingThresholdTemperature = this.fToC(parseFloat(json.heatSetpoint));
 				}
 				return callback(null, this.HeatingThresholdTemperature);
@@ -312,9 +338,9 @@ Thermostat.prototype = {
 			if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.FAHRENHEIT) {
 				value = this.cToF(value);
 			}
-			this.log('Setting target heat threshold from: ', this.apiAddress + '/config');
+			this.log('Setting target heat threshold from: ', this.apiEndpoint + '/zone/1/config');
 			return request.put({
-				url: this.apiAddress + '/config',
+				url: this.apiEndpoint + '/zone/1/config',
 				json: {
 					heatSetpoint: value
 				},
@@ -334,9 +360,9 @@ Thermostat.prototype = {
 	},
 
 	getCoolingThresholdTemperature: function(callback) {
-			this.log('Getting cool threshold from: ', this.apiAddress + '/config');
+			this.log('Getting cool threshold from: ', this.apiEndpoint + '/zone/1/config');
 			return request.get({
-				url: this.apiAddress + '/config',
+				url: this.apiEndpoint + '/zone/1/config',
 				auth: {
 					user: this.username,
 					pass: this.password
@@ -346,12 +372,12 @@ Thermostat.prototype = {
 				if (!err && response.statusCode === 200) {
 					this.log('response success');
 					json = JSON.parse(body);
-					if (json.units == 0) {
-						this.log('Target cool threshold in degrees Celsius is %s (%s)', json.coolSetpoint, json.units);
+					if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.CELSIUS) {
+						this.log('Target cool threshold in degrees Celsius is %s (%s)', json.coolSetpoint, this.TemperatureDisplayUnits);
 						this.CoolingThresholdTemperature = parseFloat(json.coolSetpoint);
 					}
-					else if (json.units == 1) {
-						this.log('Target Cool Threshold in degrees Fahrenheit is %s (%s)', json.coolSetpoint, json.units);
+					else if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.FAHRENHEIT) {
+						this.log('Target Cool Threshold in degrees Fahrenheit is %s (%s)', json.coolSetpoint, this.TemperatureDisplayUnits);
 						this.CoolingThresholdTemperature = this.fToC(parseFloat(json.coolSetpoint));
 					}
 					return callback(null, this.CoolingThresholdTemperature);
@@ -366,9 +392,9 @@ Thermostat.prototype = {
 			if (this.TemperatureDisplayUnits == Characteristic.TemperatureDisplayUnits.FAHRENHEIT) {
 				value = this.cToF(value);
 			}
-			this.log('Setting target cool threshold from: ', this.apiAddress + '/config');
+			this.log('Setting target cool threshold from: ', this.apiEndpoint + '/zone/1/config');
 			return request.put({
-				url: this.apiAddress + '/config',
+				url: this.apiEndpoint + '/zone/1/config',
 				json: {
 					coolSetpoint: value
 				},
@@ -388,38 +414,38 @@ Thermostat.prototype = {
 	},
 
 	getTemperatureDisplayUnits: function(callback) {
-			this.log('Getting temperature display units from: ', this.apiAddress + '/config');
-			// return request.get({
-			// 	url: this.apiAddress + '/config',
-			// 	auth: {
-			// 		user: this.username,
-			// 		pass: this.password
-			// 	}
-			// }, (function(err, response, body) {
-			// 	var json;
-			// 	if (!err && response.statusCode === 200) {
-			// 		this.log('response success');
-			// 		json = JSON.parse(body);
-			// 		if (json.units == 0) {
-			// 			this.TemperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.CELSIUS;
-			// 			this.log('Temperature Display Units is degrees Celsius');
-			// 		}
-			// 		else if (json.units == 1) {
+			this.log('Getting temperature display units from: ', this.apiEndpoint + '/tstat/settings');
+			return request.get({
+				url: this.apiEndpoint + '/tstat/settings',
+				auth: {
+					user: this.username,
+					pass: this.password
+				}
+			}, (function(err, response, body) {
+				var json;
+				if (!err && response.statusCode === 200) {
+					this.log('response success');
+					json = JSON.parse(body);
+					if (json.TempUnits == 1) { // 1 is "Metric"
+						this.TemperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.CELSIUS;
+						this.log('Temperature Display Units is degrees Celsius');
+					}
+					else if (json.TempUnits == 0) { // 0 is "English"
 						this.TemperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.FAHRENHEIT;
 						this.log('Temperature display units is degrees Fahrenheit');
-			// 		}
+					}
 					return callback(null, this.TemperatureDisplayUnits);
-			// 	} else {
-			// 		this.log('Error getting temperature display units', err);
-			// 		return callback("Error getting temperature display units: " + err);
-			// 	}
-			// }).bind(this));
+				} else {
+					this.log('Error getting temperature display units', err);
+					return callback("Error getting temperature display units: " + err);
+				}
+			}).bind(this));
 	},
 
 	setTemperatureDisplayUnits: function(value, callback) {
-		this.log('Setting temperature display units from/to ', this.TemperatureDisplayUnits, Characteristic.TemperatureDisplayUnits.FAHRENHEIT);
+		this.log('Setting temperature display units not implemented');
 		// return request.post({
-		// 	url: this.apiAddress + '/units/' + value,
+		// 	url: this.apiEndpoint + '/units/' + value,
 		// 	auth: {
 		// 		user: this.username,
 		// 		pass: this.password
@@ -427,7 +453,7 @@ Thermostat.prototype = {
 		// }, (function(err, response, body) {
 		// 	if (!err && response.statusCode === 200) {
 		// 		this.log('response success');
-				this.TemperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.FAHRENHEIT;
+				this.TemperatureDisplayUnits = value;
 				return callback(null);
 			// } else {
 			// 	this.log('Error getting state: %s', err);
@@ -437,9 +463,9 @@ Thermostat.prototype = {
 	},
 
 	getCurrentRelativeHumidity: function(callback) {
-		this.log('Getting current relative humidity from:', this.apiAddress + '/config');
+		this.log('Getting current relative humidity from:', this.apiEndpoint + '/zone/1/config');
 		return request.get({
-			url: this.apiAddress + '/config',
+			url: this.apiEndpoint + '/zone/1/config',
 			auth: {
 				user: this.username,
 				pass: this.password
@@ -460,9 +486,9 @@ Thermostat.prototype = {
 	 },
 
 	 getCurrentFanState: function(callback) {
-		this.log('Getting current fan state from:', this.apiAddress + '/config');
+		this.log('Getting current fan state from:', this.apiEndpoint + '/zone/1/config');
 		return request.get({
-			url: this.apiAddress + '/config',
+			url: this.apiEndpoint + '/zone/1/config',
 			auth: {
 				user: this.username,
 				pass: this.password
@@ -496,6 +522,43 @@ Thermostat.prototype = {
 		}).bind(this));
 	 },
 
+	getFanActive: function(callback) {
+		this.log('Getting current fan state from:', this.apiEndpoint + '/zone/1/config');
+		return request.get({
+			url: this.apiEndpoint + '/zone/1/config',
+			auth: {
+				user: this.username,
+				pass: this.password
+			}
+		}, (function(err, response, body) {
+			var json;
+			if (!err && response.statusCode === 200) {
+				this.log('response success');
+				json = JSON.parse(body);
+				this.log('Current fan state is %s', json.fanMode);
+
+				switch (json.fanMode) {
+					case "off":
+						this.CurrentFanActive = Characteristic.Active.INACTIVE;
+						break;
+					case "auto":
+					case "low":
+					case "medium":
+					case "high":
+					this.CurrentFanActive = Characteristic.Active.ACTIVE;
+					break;
+					default:
+						this.CurrentFanActive = Characteristic.Active.INACTIVE;
+						break;
+				}
+				return callback(null, this.CurrentFanActive);
+			} else {
+			this.log('Error getting current fan state: %s', err);
+				return callback("Error getting current fan state: " + err);
+			}
+		}).bind(this));
+	},
+
  	getTargetRelativeHumidity: function(callback) {
 		var error;
 		this.log('Get humidity unsupported');
@@ -525,6 +588,14 @@ Thermostat.prototype = {
 		.setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
 		.setCharacteristic(Characteristic.Model, this.model)
 		.setCharacteristic(Characteristic.SerialNumber, this.serial_number);
+
+		this.FanService
+		.getCharacteristic(Characteristic.Active)
+		.on('get', this.getFanActive.bind(this));
+
+		this.FanService
+		.getCharacteristic(Characteristic.CurrentFanState)
+		.on('get', this.getCurrentFanState.bind(this));
 
 		this.ThermostatService
 		.getCharacteristic(Characteristic.CurrentHeatingCoolingState)
@@ -572,7 +643,7 @@ Thermostat.prototype = {
 		.setProps({
 			maxValue: 100,
 			minValue: 0,
-			minStep: 1
+			minStep: .1
 		});
 
 		this.ThermostatService
@@ -586,16 +657,16 @@ Thermostat.prototype = {
 		this.ThermostatService
 		.getCharacteristic(Characteristic.HeatingThresholdTemperature)
 		.setProps({
-			maxValue: 35,
-			minValue: 0,
+			maxValue: this.maxTemp,
+			minValue: this.minTemp,
 			minStep: 0.5
 		});
 
 		this.ThermostatService
 		.getCharacteristic(Characteristic.CoolingThresholdTemperature)
 		.setProps({
-			maxValue: 35,
-			minValue: 0,
+			maxValue: this.maxTemp,
+			minValue: this.minTemp,
 			minStep: 0.5
 		});
 
